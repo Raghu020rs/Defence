471b0492be004f38f705d8b75616f7d99413d48d
Raghu Chauhan <rsc2062003@gmail.com>
2025-10-10 12:16:01 +0530

    Day_7

---
+++ Frontend/JavaScript/Day_7/script.js
@@ -0,0 +1,11 @@
+var a = 10;
+var b = 20;
+
+function addnum(num1 , num2){
+  var sum  = num1+ num2;
+  return sum;
+}
+
+var sumResult1 = addnum(a,b);
+var sumResult2 = addnum(10,20);
+console.log(sumResult1, sumResult2);;
