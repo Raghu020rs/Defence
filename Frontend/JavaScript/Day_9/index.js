02d80e5507e20bacd05cb13866e6d7d718d03f6a
Raghu Chauhan <rsc2062003@gmail.com>
2025-10-14 01:11:15 +0530

    updated Day_9

--- Frontend/JavaScript/Day_9/index.js
+++ Frontend/JavaScript/Day_9/index.js
@@ -1,5 +1,62 @@
-const arr = [10, 20, 30, "Rohit", 90];
-//For each 
-arr.forEach((number)=> {
-  console.log(number);
+// const arr = [10, 20, 30, "Rohit", 90];
+// //For each 
+// arr.forEach((number)=> {
+//   console.log(number);
+// });
+
+
+const products = [
+  {
+    id: 1,
+    name: "laptop",
+    category: "electronics",
+    price: 1200,
+    inStock: true
+  },
+  {
+    id: 2, 
+    name: "Book",
+    category: "education",
+    price: 30,
+    inStock: true
+  },
+  {id: 3,
+    name: "Phone",
+    category: "appliances",
+    price: 1500,
+    inStock: false
+  },
+  {
+    id: 4,
+    name: "Headphones",
+    category: "Electronics",
+    price: 100,
+    inStock: true
+  }
+];
+
+console.log(" Our Products");
+products.forEach(product => {
+  console.log(`- ${product.name}`);
 });
+
+products.forEach((products, index) => {
+  console.log(`${index + 1}. ${products.name}`);
+});
+
+
+const productsNames = products.map(products => {
+  return products.name;
+})
+
+console.log(productsNames);
+
+// console.log(products);
+
+const availability = products.filter(products => {
+  return products.inStock === true && products.category === "Electronics";
+});
+
+console.log(availability);
+
+
