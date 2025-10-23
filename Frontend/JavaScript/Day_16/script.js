7aa2421f311f76150a77dda51ab84bcf290a374b
Raghu Chauhan <rsc2062003@gmail.com>
2025-10-24 01:34:47 +0530

    Todo app

---
+++ Frontend/JavaScript/Day_16/script.js
@@ -0,0 +1,17 @@
+console.log('Hi ji ');
+const button1 = document.getElementById("button1");
+button1.addEventListener('click', () => {
+  console.log("Button 1 is clicked");
+});
+
+const button2 = document.getElementById("button2");
+button2.addEventListener('click', () => {
+  console.log("Button 2 is clicked");
+});
+
+const button3 = document.getElementById("button3");
+button3.addEventListener('click', () => {
+  console.log("Button 3 is clicked");
+});
+
+console.log("I am the end ");
